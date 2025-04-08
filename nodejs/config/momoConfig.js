fafea14f9fa077b8efd6de815b6617bdda0af252
phamminh12 <aeonepiece7a4@gmail.com>
2025-04-08 23:23:33 +0700

    payment method

--- nodejs/config/momoConfig.js
+++ nodejs/config/momoConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-    partnerCode: "YOUR_PARTNER_CODE",
-    accessKey: "YOUR_ACCESS_KEY",
-    secretKey: "YOUR_SECRET_KEY",
+    partnerCode: "MOMO",
+    accessKey: "F8BBA842ECF85",
+    secretKey: "K951B6PE1waDMi640xX08PD3vg6EkVlz",
     endpoint: "https://test-payment.momo.vn/v2/gateway/api/create",
     returnUrl: "http://localhost:3000/payment/return",
     notifyUrl: "http://localhost:3000/payment/notify",
--- nodejs/controller/customer/paymentController.js
+++ nodejs/controller/customer/paymentController.js
@@ -1,42 +1,79 @@
 const axios = require("axios");
 const crypto = require("crypto");
 const momoConfig = require("../../config/momoConfig");
+const sanphamDB = require('../../model/customer/sanphamDB');
 
 exports.createPayment = async (req, res) => {
   try {
-    const { amount, orderId, orderInfo } = req.body;
+    const { total } = req.body;
 
-    const requestId = `${orderId}-${Date.now()}`;
-    const rawSignature = `accessKey=${momoConfig.accessKey}&amount=${amount}&extraData=&ipnUrl=${momoConfig.notifyUrl}&orderId=${orderId}&orderInfo=${orderInfo}&partnerCode=${momoConfig.partnerCode}&redirectUrl=${momoConfig.returnUrl}&requestId=${requestId}&requestType=captureWallet`;
+    amount = total;
+    orderInfo = "Pay with MoMo";
+
+    const partnerCode = momoConfig.partnerCode;
+    const accessKey = momoConfig.accessKey;
+    const secretKey = momoConfig.secretKey;
+    const redirectUrl = momoConfig.returnUrl;
+    const ipnUrl = momoConfig.notifyUrl;
+
+    const requestType = "payWithMethod";
+    const orderId = `${partnerCode}${Date.now()}`;
+    const requestId = orderId;
+    const extraData = "";
+    const orderGroupId = "";
+    const autoCapture = true;
+    const lang = "vi";
+
+    const rawSignature = 
+      `accessKey=${accessKey}&amount=${amount}&extraData=${extraData}&ipnUrl=${ipnUrl}` +
+      `&orderId=${orderId}&orderInfo=${orderInfo}&partnerCode=${partnerCode}` +
+      `&redirectUrl=${redirectUrl}&requestId=${requestId}&requestType=${requestType}`;
 
     const signature = crypto
-      .createHmac("sha256", momoConfig.secretKey)
+      .createHmac("sha256", secretKey)
       .update(rawSignature)
       .digest("hex");
 
     const requestBody = {
-      partnerCode: momoConfig.partnerCode,
-      accessKey: momoConfig.accessKey,
+      partnerCode,
+      partnerName: "Test",
+      storeId: "MomoTestStore",
       requestId,
       amount,
       orderId,
       orderInfo,
-      redirectUrl: momoConfig.returnUrl,
-      ipnUrl: momoConfig.notifyUrl,
-      extraData: "",
-      requestType: "captureWallet",
+      redirectUrl,
+      ipnUrl,
+      lang,
+      requestType,
+      autoCapture,
+      extraData,
+      orderGroupId,
       signature,
     };
 
-    const response = await axios.post(momoConfig.endpoint, requestBody);
+    const options = {
+      method: "POST",
+      url: momoConfig.endpoint,
+      headers: {
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0",
+      },
+      data: requestBody,
+    };
+
+    const response = await axios(options);
 
     if (response.data.resultCode === 0) {
+      sanphamDB.orderCart((data) => {}, req.body, req.cookies.dataLogin.ID);
       res.redirect(response.data.payUrl);
     } else {
       res.status(400).json({ message: "Payment creation failed", error: response.data });
     }
+
   } catch (error) {
-    res.status(500).json({ message: "Internal server error", error });
+    console.error("Payment error:", error);
+    res.status(500).json({ message: "Internal server error", error: error.message });
   }
 };
 
@@ -44,7 +81,7 @@ exports.paymentReturn = (req, res) => {
   const { resultCode } = req.query;
 
   if (resultCode === "0") {
-    res.render("customer/paymentSuccess", { message: "Payment successful!" });
+    res.redirect('/customer/home');
   } else {
     res.render("customer/paymentFailure", { message: "Payment failed!" });
   }
